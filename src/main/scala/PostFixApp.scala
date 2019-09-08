1be0bdd2ce14fb716cff4eda14e23db93cf3b0f3
benjam60 <erichseb@umich.edu>
2019-09-08 00:24:27 -0400

    failing swap test case

--- src/main/scala/PostFixApp.scala
+++ src/main/scala/PostFixApp.scala
@@ -2,6 +2,10 @@ package ScalaPostFixLanguage
 
 object PostFixApp extends App {
 	assert(PostFixInterpreter.run("0") == "0")
+	assert(PostFixInterpreter.run("0 1") == "1")
+	assert(PostFixInterpreter.run("0 1 2 pop") == "1")
+	assert(PostFixInterpreter.run("0 1 2 pop pop") == "0")
+	assert(PostFixInterpreter.run("1 2 swap 3 pop") == "1")
 
 }
 
