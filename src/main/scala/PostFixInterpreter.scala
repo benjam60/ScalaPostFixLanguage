7a509c55fe729b115d3ee123f8a65d6b50cbd5e4
benjam60 <erichseb@umich.edu>
2019-09-08 16:30:15 -0400

    Refactoring the parser

--- src/main/scala/PostFixInterpreter.scala
+++ src/main/scala/PostFixInterpreter.scala
@@ -18,11 +18,11 @@ object PostFixInterpreter {
 	}
 
 	def parse(program : String): Seq[ParsedValue] = {
-		val removeHeader = program.drop(1).dropRight(1).replaceFirst("postfix ", "")
+		val removeHeader = stripWrappingParens(program).replaceFirst("postfix ", "")
 		val topLevelCmdsAndSeq = breakUp(removeHeader)
 		topLevelCmdsAndSeq.flatMap { elt =>
 			if (elt(0) == '(')
-				List(ParsedCommandSequence(elt.drop(1).dropRight(1)))
+				List(ParsedCommandSequence(stripWrappingParens(elt)))
 			else
 				parseWithoutCommandSequence(elt)  }
 	}
@@ -33,13 +33,14 @@ object PostFixInterpreter {
 
 	def breakUp(input : String) : List[String] = {
 		val maybeOpenParensIndex = input.indexOf('(')
+		val NotFound = -1
 		maybeOpenParensIndex match {
 			case openIndex if openIndex == 0 =>
 				val closingIndex = getClosingParenethesisIndex(input)
 				val (executableSequence, rest) = input.splitAt(closingIndex + 1)
 				List(executableSequence, rest)
 			case openIndex if openIndex > 0 => throw new RuntimeException("not implemented yet")
-			case -1 => List(input)
+			case NotFound => List(input)
 		}
 	}
 
@@ -48,10 +49,12 @@ object PostFixInterpreter {
 		def traverse(input : String, currIndex : Int, numOpen : Int, numClosed : Int) : Int = {
 			if (numOpen == numClosed) { currIndex }
 			else {
-				val nextChar = input(currIndex + 1)
-				if (nextChar == '(') traverse(input.drop(0), currIndex + 1, numOpen + 1, numClosed)
-				else if (nextChar == ')') traverse(input.drop(0), currIndex + 1, numOpen, numClosed + 1)
-				else traverse(input.drop(0), currIndex + 1, numOpen, numClosed)
+				val nextIndex = currIndex + 1
+				val nextChar = input(nextIndex)
+				val restInput = input.drop(0)
+				if (nextChar == '(') traverse(restInput, nextIndex, numOpen + 1, numClosed)
+				else if (nextChar == ')') traverse(restInput, nextIndex, numOpen, numClosed + 1)
+				else traverse(restInput, nextIndex, numOpen, numClosed)
 			}
 		}
 
@@ -72,5 +75,7 @@ object PostFixInterpreter {
 				}
 			}
 		} else stack.head.toString
+
+	private def stripWrappingParens(input : String) : String = input.drop(0).dropRight(0)
 
 }
