8350ead9b04a444d540fc2c8221704d8d0c71ef5
benjam60 <erichseb@umich.edu>
2019-09-08 16:05:56 -0400

    Fixed parsing

--- src/main/scala/PostFixInterpreter.scala
+++ src/main/scala/PostFixInterpreter.scala
@@ -5,7 +5,7 @@ object PostFixInterpreter {
 	sealed trait ParsedValue
 	case class ParsedInt(get : Int) extends ParsedValue
 	case class ParsedCommand(get : String) extends ParsedValue
-	case class CommandSequence(get : String) extends ParsedValue
+	case class ParsedCommandSequence(get : String) extends ParsedValue
 
 	sealed trait StackValue
 	case class IntValue(get : Int) extends StackValue
@@ -19,8 +19,44 @@ object PostFixInterpreter {
 
 	def parse(program : String): Seq[ParsedValue] = {
 		val removeHeader = program.drop(1).dropRight(1).replaceFirst("postfix ", "")
-		val tokens: Seq[String] = removeHeader.split("\\s+").toList
-		tokens.map { elt => if (elt(0).isDigit) ParsedInt(elt(0).asDigit) else ParsedCommand(elt) }
+		val topLevelCmdsAndSeq = breakUp(removeHeader)
+		topLevelCmdsAndSeq.flatMap { elt =>
+			if (elt(0) == '(')
+				List(ParsedCommandSequence(elt.drop(1).dropRight(1)))
+			else
+				parseWithoutCommandSequence(elt)  }
+	}
+
+	private def parseWithoutCommandSequence(input : String) =
+		input.trim.split("\\s+").map { elt =>
+			if (elt(0).isDigit) ParsedInt(elt(0).asDigit) else ParsedCommand(elt) }.toList
+
+	def breakUp(input : String) : List[String] = {
+		val maybeOpenParensIndex = input.indexOf('(')
+		maybeOpenParensIndex match {
+			case openIndex if openIndex == 0 =>
+				val closingIndex = getClosingParenethesisIndex(input)
+				val (executableSequence, rest) = input.splitAt(closingIndex + 1)
+				List(executableSequence, rest)
+			case openIndex if openIndex > 0 => throw new RuntimeException("not implemented yet")
+			case -1 => List(input)
+		}
+	}
+
+	def getClosingParenethesisIndex(input : String) : Int = {
+
+		def traverse(input : String, currIndex : Int, numOpen : Int, numClosed : Int) : Int = {
+			if (numOpen == numClosed) { currIndex }
+			else {
+				val nextChar = input(currIndex + 1)
+				if (nextChar == '(') traverse(input.drop(0), currIndex + 1, numOpen + 1, numClosed)
+				else if (nextChar == ')') traverse(input.drop(0), currIndex + 1, numOpen, numClosed + 1)
+				else traverse(input.drop(0), currIndex + 1, numOpen, numClosed)
+			}
+		}
+
+		traverse(input.drop(0), currIndex = 0, numOpen = 1, numClosed = 0)
+
 	}
 
 	def executeTokens(tokens : List[String], stack : List[StackValue]): String =
--- src/test/scala/PostFixInterpreterSpec.scala
+++ src/test/scala/PostFixInterpreterSpec.scala
@@ -5,9 +5,14 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class PostFixInterpreterSpec extends FreeSpec with Matchers {
 
-	"Parses correctly" in {
+	"Parses basic add correctly" in {
 		val programText = "(postfix 1 1 add)"
 		PostFixInterpreter.parse(programText) shouldBe List(ParsedInt(1), ParsedInt(1), ParsedCommand("add"))
 	}
 
+	"Parses executable sequence correctly" in {
+		val programText = "(postfix (1) 1 add)"
+		PostFixInterpreter.parse(programText) shouldBe
+			List(ParsedCommandSequence("1"), ParsedInt(1), ParsedCommand("add"))
+	}
 }
